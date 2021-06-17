ec768b4384c03d3bab47257eccc80130035337bc
Rahul Ranjan <rahul.ranjan@cybg.com>
2021-06-18 00:38:37 +0100

    Added cors origin

--- src/main/java/com/ranjanenterprises/controller/UserController.java
+++ src/main/java/com/ranjanenterprises/controller/UserController.java
@@ -6,6 +6,7 @@ import com.ranjanenterprises.representation.UserResponse;
 import com.ranjanenterprises.representation.Users;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class UserController {
 
